4f016d68f7b6bed1e8a4a4c2c0bfed8190e5c3b6
github username <juliannne@gmail.com>
2018-01-01 18:23:20 -0500

    Create models for user, location, calendar

---
+++ db/migrate/20180101231226_create_calendars.rb
@@ -0,0 +1,10 @@
+class CreateCalendars < ActiveRecord::Migration[5.1]
+  def change
+    create_table :calendars do |t|
+      t.integer :user_id
+      t.string :name
+
+      t.timestamps null: false
+    end
+  end
+end
