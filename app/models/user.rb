83a6fbda9b0b2df5182a11567466a931aade692c
github username <juliannne@gmail.com>
2018-01-01 22:35:41 -0500

    Change name to username

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   has_many :locations
 
   has_secure_password
-  validates :name, presence: true
-  validates :name, uniqueness: true
+  validates :username, presence: true
+  validates :username, uniqueness: true
 end
