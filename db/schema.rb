2f4aede3d534f369fe3dc85cf617e77fb27117e8
github username <juliannne@gmail.com>
2018-01-02 10:12:28 -0500

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180101232801) do
+ActiveRecord::Schema.define(version: 20180102150722) do
 
   create_table "calendars", force: :cascade do |t|
     t.integer "user_id"
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 20180101232801) do
     t.string "note"
     t.string "creator"
     t.integer "location_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "invites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "event_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
