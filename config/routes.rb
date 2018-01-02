51e84a76ec2610204f17cb6236ab863ee48d7397
github username <juliannne@gmail.com>
2018-01-01 21:59:39 -0500

    Add routes for users, sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'users#index'
+  resources :users, only: [:index, :create]
+  get '/signup' => 'users#new'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
 end
