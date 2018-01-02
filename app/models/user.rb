5c825250a465d78b6881569dfb7d2e3f22a643df
github username <juliannne@gmail.com>
2018-01-02 02:20:43 -0500

    Create methods to sort events by date, select today's events

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ApplicationRecord
   has_secure_password
   validates :username, presence: true
   validates :username, uniqueness: true
+
+  def sort_by_date
+    self.calendar.events.sort { |a, b| a.start_time.to_date <=> b.start_time.to_date }
+  end
+
+  def today_events
+    self.calendar.events.select { |event| event.start_time.to_date == Date.today }
+  end
 end
