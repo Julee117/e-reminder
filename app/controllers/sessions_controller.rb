88a98154aa3a2b72ddca5279db26f1eb888cf7e9
github username <juliannne@gmail.com>
2018-01-01 22:07:22 -0500

    Create sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      @user = user
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    if session[:user_id]
+      session.delete(:user_id)
+      redirect_to "/login"
+    else
+      redirect_to root_path
+    end
+  end
+end
