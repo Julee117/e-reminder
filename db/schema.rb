2eade757f0b2098f905bc9d2d3161594380f63ca
github username <juliannne@gmail.com>
2018-01-02 20:21:45 -0500

    Add email to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180102150722) do
+ActiveRecord::Schema.define(version: 20180102215100) do
 
   create_table "calendars", force: :cascade do |t|
     t.integer "user_id"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20180102150722) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
