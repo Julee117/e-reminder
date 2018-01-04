ff517a29eb36312a928a44ff2af4c48d11984210
github username <juliannne@gmail.com>
2018-01-03 23:29:35 -0500

    Add uid to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180103063448) do
+ActiveRecord::Schema.define(version: 20180104040320) do
 
   create_table "calendars", force: :cascade do |t|
     t.integer "user_id"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 20180103063448) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email"
+    t.string "uid"
   end
 
 end
