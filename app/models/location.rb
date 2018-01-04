f566edc55780194c942bf120521f7887e0564091
github username <juliannne@gmail.com>
2018-01-04 11:39:47 -0500

    Add validations to location

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,9 @@
 class Location < ApplicationRecord
-  has_many :events
+  has_many :events, dependent: :destroy
+
+  validates :name, presence: true
+  validates :street_address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zipcode, presence: true
 end
