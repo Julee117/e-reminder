832d3252805f47bd1c54ca5faca1c7d2f0acb45e
github username <juliannne@gmail.com>
2018-01-03 23:59:27 -0500

    Updated method to signup through facebook

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,30 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(username: params[:username])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      @user = user
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
       redirect_to users_path
     else
       render :new
+    end
+  end
+
+  def facebook
+    if auth = request.env["omniauth.auth"]
+      if @user = User.find_by(uid: auth["uid"])
+        session[:user_id] = @user.id
+        redirect_to users_path
+      else
+        @user = User.new(email: oauth_email, username: oauth_name, password: SecureRandom.hex)
+        if @user.save
+          session[:user_id] = @user.id
+          @calendar = @user.create_calendar(name: @user.username)
+          redirect_to users_path
+        else
+          render :new
+        end
+      end
     end
   end
 
@@ -20,5 +37,19 @@ class SessionsController < ApplicationController
     else
       redirect_to users_path
     end
+  end
+
+  private
+
+  def auth
+    request.env["omniauth.auth"]
+  end
+
+  def oauth_email
+    auth["info"]["email"]
+  end
+
+  def oauth_name
+    oauth_email.split("@").first
   end
 end
