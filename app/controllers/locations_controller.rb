bbaa17890fbe73eaca9a5fe2d7017ccc7e8a7dc2
github username <juliannne@gmail.com>
2018-01-04 23:54:10 -0500

    Create show and popular methods

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,4 +1,12 @@
 class LocationsController < ApplicationController
   def index
   end
+
+  def show
+    @location = Location.find(params[:id])
+  end
+
+  def popular
+    @locations = Location.most_popular
+  end
 end
