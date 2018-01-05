67d76c33b5d89fb77dba8920430b457cd0b24a9c
github username <juliannne@gmail.com>
2018-01-04 23:15:18 -0500

    Add custom location_attributes method and validation method

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,12 +1,22 @@
 class Event < ApplicationRecord
-  belongs_to :location, optional: true
-  has_many :notifications
+  belongs_to :location
+  has_many :notifications, dependent: :destroy
   has_many :calendars, through: :notifications
-  has_many :invites
+  has_many :invites, dependent: :destroy
   has_many :users, through: :invites
 
   validates :title, presence: true
-  accepts_nested_attributes_for :location
+  validates :title, uniqueness: true
+  validate :end_time_is_after_start_time
+
+  def location_attributes=(location_attributes)
+    if location_id != nil
+      location = Location.find_by(id: location_id)
+      self.location = location
+    else
+      self.create_location(name: location_attributes[:name], street_address: location_attributes[:street_address], city: location_attributes[:city], state: location_attributes[:state], zipcode: location_attributes[:zipcode])
+    end
+  end
 
   def add_date_value
     self.update_attribute(:date, self.start_time)
@@ -14,6 +24,11 @@ class Event < ApplicationRecord
 
   def update_creator(name)
     self.update_attribute(:creator, name)
+  end
+
+  def add_attribute_values(name)
+    add_date_value
+    update_creator(name)
   end
 
   def find_user
@@ -27,6 +42,14 @@ class Event < ApplicationRecord
   def add_to_calendars
     find_user.each do |user|
       user.calendar.events << self
+    end
+  end
+
+  private
+
+  def end_time_is_after_start_time
+    if end_time < start_time
+      errors.add(:end_time, "cannot be before start date/time")
     end
   end
 end
