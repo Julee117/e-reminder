93edcbf26c5b20757f1af94e52de60c6e9f00f06
github username <juliannne@gmail.com>
2018-01-29 10:44:00 -0500

    Add new, create and private methods

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,28 @@
+class CommentsController < ApplicationController
+  before_action :set_event
+
+  def new
+    @comment = @event.comments.build
+  end
+
+  def create
+    @calendar = current_user.calendar
+    @comment = @event.comments.build(comment_params)
+    @comment.user = current_user
+    if @comment.save
+      redirect_to calendar_event_path(@calendar.name, @event)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_event
+    @event = Event.find(params[:event_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content, :event_id, :user_id)
+  end
+end
