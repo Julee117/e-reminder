4f9404bcac007b52d5b4477faedba577a69c9b42
github username <juliannne@gmail.com>
2018-01-29 22:56:58 -0500

    Create AJAX post method

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,12 +1,30 @@
 $(function() {
   $(".all-comments").on('click', function(e) {
-    e.preventDefault()
+    e.preventDefault();
     $.get(this.href + ".json").success(function(response) {
       const $div = $("div.comments")
+      $div.fadeToggle()
       $div.html("")
       response.forEach(function(comment) {
-        $div.append('<div class="comments-show">' + '<strong>' + comment.user.username + '</strong>: ' + comment.content + '</div><br>')
+        if (comment.content != "") {
+          $div.append('<div class="comments-show">' + '<strong>' + comment.user.username.charAt(0).toUpperCase() + comment.user.username.slice(1) + '</strong>: ' + comment.content + '</div><br>')
+        }
       })
     })
+  })
+
+  $("#new_comment").on("submit", function(e) {
+    e.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: this.action,
+      data: $(this).serialize(),
+      success: function(response) {
+        $("#comment_content").val("");
+        if (response.content != "") {
+          $div.append('<div class="comments-show">' + '<strong>' + response.user.username + '</strong>: ' + response.content + '</div><br>')
+        }
+      }
+    });
   })
 })
