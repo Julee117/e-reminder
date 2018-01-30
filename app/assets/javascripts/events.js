0f971b3081c31deb0da1653cd1072a53db22de68
github username <juliannne@gmail.com>
2018-01-30 12:53:16 -0500

    Add prototype functions to convert time and display address

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -6,7 +6,6 @@ $(function() {
       $('.event-show').html("")
       let newEvent = new Event(data)
       let event = newEvent.displayEvent()
-
       $('.event-show').append(event)
     })
   })
@@ -18,6 +17,8 @@ $(function() {
     this.start_time = event.start_time
     this.end_time = event.end_time
     this.creator = event.creator
+    this.users = event.users
+    this.location = event.location
   }
 
   Event.prototype.convertToDate = function() {
@@ -26,6 +27,27 @@ $(function() {
     eventDate = new Date(parts[0], parts[1] - 1, parts[2]);
     dateParts = eventDate.toDateString().split(" ");
     return `${dateParts[1]} ${dateParts[2]}, ${dateParts[3]}`
+  }
+
+  Event.prototype.convertStartTime = function() {
+    dateFormat = new Date(`${this.start_time}`);
+    timeString = dateFormat.toLocaleTimeString();
+    parts = timeString.split(":");
+    return `${parts[0]}:${parts[1]} ${parts[2].slice(3)}`
+  }
+
+  Event.prototype.convertEndTime = function() {
+    dateFormat = new Date(`${this.end_time}`);
+    timeString = dateFormat.toLocaleTimeString();
+    parts = timeString.split(":");
+    return `${parts[0]}:${parts[1]} ${parts[2].slice(3)}`
+  }
+
+  Event.prototype.displayAddress = function() {
+    let addressHtml = `
+    <p>Address: ${this.location.street_address} ${this.location.city} ${this.location.state} ${this.location.zipcode}<p>
+    `
+    return addressHtml
   }
 
 })
