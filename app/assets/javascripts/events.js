cfdb0632954af41d119526222d964b9e8439ef43
github username <juliannne@gmail.com>
2018-01-30 11:50:19 -0500

    Add prototype to convert to date

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -19,4 +19,13 @@ $(function() {
     this.end_time = event.end_time
     this.creator = event.creator
   }
+
+  Event.prototype.convertToDate = function() {
+    splitDateTime = `${this.date}`.split("T");
+    parts = splitDateTime[0].split("-");
+    eventDate = new Date(parts[0], parts[1] - 1, parts[2]);
+    dateParts = eventDate.toDateString().split(" ");
+    return `${dateParts[1]} ${dateParts[2]}, ${dateParts[3]}`
+  }
+
 })
