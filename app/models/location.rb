5ee3389b2fec5a737c5d4de00079c2952008f803
github username <juliannne@gmail.com>
2018-01-31 09:47:48 -0500

    Add next method

--- app/models/location.rb
+++ app/models/location.rb
@@ -10,4 +10,15 @@ class Location < ApplicationRecord
   def self.most_popular
     joins(:events).group(:id).having("COUNT(*) > 1").order("COUNT(*) DESC").limit(5)
   end
+
+  def next
+    locations = Location.most_popular
+    selected_loc = locations.select { |loc| loc.events.length < self.events.length }
+    if selected_loc == []
+      next_location = locations.select { |loc| loc.events.length <= self.events.length && loc.id > self.id }.first
+    else
+      next_location = selected_loc.first
+    end
+    next_location ||= locations.first
+  end
 end
