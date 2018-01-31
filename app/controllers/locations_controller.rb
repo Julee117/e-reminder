6ebf02e6c9a9bf688c5245c639d869150f68584b
github username <juliannne@gmail.com>
2018-01-31 09:47:23 -0500

    Add next and prev methods

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,14 +1,25 @@
 class LocationsController < ApplicationController
+  before_action :set_location, only: [:show, :next, :prev]
+
   def index
     @user_locations = current_user.today_events
   end
 
   def show
-    @location = Location.find(params[:id])
     respond_to do |format|
       format.html
       format.json {render json: @location}
-    end 
+    end
+  end
+
+  def next
+    @next_location = @location.next
+    render json: @next_location
+  end
+
+  def prev
+    @prev_location = @location.prev
+    render json: @prev_location
   end
 
   def popular
@@ -17,5 +28,11 @@ class LocationsController < ApplicationController
       format.html
       format.json {render json: @locations}
     end
+  end
+
+  private
+
+  def set_location
+    @location = Location.find(params[:id])
   end
 end
