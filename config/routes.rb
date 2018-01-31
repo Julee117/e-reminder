4696fb1b7fba9981ddd401040768744872e04923
github username <juliannne@gmail.com>
2018-01-31 10:05:14 -0500

    Add routes for locations#next and locations#prev

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   get '/locations/most_popular' => 'locations#popular'
   get '/calendars/:calendar_name/events/:id/next' => 'events#next'
   get '/calendars/:calendar_name/events/:id/prev' => 'events#prev'
+  get '/locations/:id/next' => 'locations#next'
+  get '/locations/:id/prev' => 'locations#prev'
   resources :calendars, param: :name do
     resources :events
   end
