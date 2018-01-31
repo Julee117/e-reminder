3771f4ad8ee49c444074a2e01ef8cdd429359d3f
github username <juliannne@gmail.com>
2018-01-31 00:20:55 -0500

    Add respond_to methods to show and popular methods

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,12 +1,21 @@
 class LocationsController < ApplicationController
   def index
+    @user_locations = current_user.today_events
   end
 
   def show
     @location = Location.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json {render json: @location}
+    end 
   end
 
   def popular
     @locations = Location.most_popular
+    respond_to do |format|
+      format.html
+      format.json {render json: @locations}
+    end
   end
 end
